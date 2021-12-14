da88db61587621b4ce66f414b78e4d7fbcfd94c0
dumbus <dumbusx@gmail.com>
2021-12-14 21:10:08 +0300

    feat: add favourite toys

--- src/ts/modules/toys.ts
+++ src/ts/modules/toys.ts
@@ -2,13 +2,43 @@ import data from '../../data.json';
 import { Card } from './cards';
 
 const toys = () => {
-  const toysArr: HTMLElement[] = [];
+  const toysHTMLArr: HTMLElement[] = [];
+  const toysObjArr: Card[] = [];
 
-  for (let i = 0; i < data.length; i++) {
-    const card = new Card(data[i]);
-    toysArr[i] = card.createCard();
-    card.insertCard();
+  // can be copied to utils:
+  let favoritesCounter = 0;
+  const headerFavorites = document.querySelector('.header-controls-favourites span');
+
+  function updateFavorites(card: HTMLElement) {
+    card.addEventListener('click', () => {
+      if (favoritesCounter === 20) {
+        // TODO: function with modal
+        console.log('more than 20');
+      } else {
+        if (card.classList.contains('active')) {
+          card.classList.remove('active');
+          favoritesCounter--;
+        } else {
+          card.classList.add('active');
+          favoritesCounter++;
+        }
+        headerFavorites.textContent = '' + favoritesCounter;
+      }
+    });
   }
+
+  function renderCards() {
+    for (let i = 0; i < data.length; i++) {
+      const card = new Card(data[i]);
+      const cardHTML = card.createCard();
+      toysHTMLArr[i] = cardHTML;
+      toysObjArr[i] = card;
+      card.insertCard();
+
+      updateFavorites(cardHTML);
+    }
+  }
+  renderCards();
 };
 
 export default toys;
---
+++ src/ts/modules/utils.ts
@@ -0,0 +1,5 @@
+// const utils = () => {
+//   function count
+// }
+
+// export default utils;
